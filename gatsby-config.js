86c43b1bdd444673f849769424c6498c8841c131
Dylan Corrales <deathcamel58@gmail.com>
2021-08-10 10:15:41 -0400

    Replace GA with Gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,9 +19,24 @@ module.exports = {
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "UA-124176321-1",
+        trackingIds: [
+          "UA-124176321-1", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: false,
+        },
       },
     },
     "gatsby-plugin-react-helmet",
