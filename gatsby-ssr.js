90d61fb7d61b50a2a6329e14dd28c90bc087a203
Dylan Corrales <deathcamel58@gmail.com>
2021-08-11 12:36:38 -0400

    Locally host `bat.js` to reduce DNS time and reuse connection

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -29,6 +29,13 @@ export function onRenderBody(
                     fbq('track', "PageView");
                 `,
             }}
+        />,
+        <script key="bing-tracking-events"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                    (function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"17508257"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.type="text/javascript",n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","/js/bat.js","uetq");
+                `,
+                }}
         />
     ]);
 }
