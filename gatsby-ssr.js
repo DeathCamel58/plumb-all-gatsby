82422debedbdc24ba9ff2bab487e6209714d70cb
Dylan Corrales <deathcamel58@gmail.com>
2021-10-13 14:53:35 -0400

    Manually inject Google Optimize
    Remove `gtag.js`, as it's not needed anymore.

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -7,6 +7,10 @@ export function onRenderBody(
     if (process.env.NODE_ENV === `production`) {
         setHeadComponents([
             <script
+                key="google-optimize"
+                src="https://www.googleoptimize.com/optimize.js?id=OPT-MH4QC29"
+            />,
+            <script
                 key="facebook-tracking-events"
                 dangerouslySetInnerHTML={{
                     __html: `
