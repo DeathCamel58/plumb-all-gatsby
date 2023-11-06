eaeb9e92051169f39e9d7cee2489e362cf965fa9
Dylan Corrales <deathcamel58@gmail.com>
2023-11-06 09:25:05 -0500

    Use Local JS files

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -14,36 +14,11 @@ export function onRenderBody(
             <Script
                 key="facebook-tracking-events"
                 strategy="post-hydrate"
-                dangerouslySetInnerHTML={{
-                    __html: `
-                    ! function(f, b, e, v, n, t, s) {
-                        if (f.fbq) return;
-                        n = f.fbq = function() {
-                            n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
-                        };
-                        if (!f._fbq) f._fbq = n;
-                        n.push = n;
-                        n.loaded = !0;
-                        n.version = '2.0';
-                        n.queue = [];
-                        t = b.createElement(e);
-                        t.async = !0;
-                        t.src = v;
-                        s = b.getElementsByTagName(e)[0];
-                        s.parentNode.insertBefore(t, s)
-                    }(window, document, 'script', '/js/fbevents.js');
-                    fbq('init', '194958962316240');
-                    fbq('track', "PageView");
-                `,
-                }}
+                src="/js/fbevents.js"
             />,
             <Script key="bing-tracking-events"
                     strategy="post-hydrate"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                    (function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"17508257"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.type="text/javascript",n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","/js/bat.js","uetq");
-                `,
-                    }}
+                    src="/js/bat.js"
             />
         ])
     }
